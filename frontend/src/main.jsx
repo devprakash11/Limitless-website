94e0bc4a6cb26d33c9bb8a5510b09b5a5522cf39
Dev Prakash <141032391+devprakash11@users.noreply.github.com>
2026-08-15 20:09:45 +0530

    create a sepearate file of each page and update the data in price

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,28 @@ import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
-import "./styles/style.css";
+// import "./styles/global.css";
+// import "./styles/pages.css";
+// import "./styles/pagesService.css";
+// import "./styles/components.css";
+import "./styles/global.css";
+import "./styles/componets/seo/ServiceContentSection.css";
+import "./styles/componets/DiscountPopup.css";
+import "./styles/componets/Footer.css";
+import "./styles/componets/Navbar.css";
+import "./styles/pages/About.css";
+import "./styles/pages/BrandingMaterials.css";
+import "./styles/pages/BusinessCardDesign.css";
+import "./styles/pages/Contact.css";
+import "./styles/pages/DownloadPreview.css";
+import "./styles/pages/Home.css";
+import "./styles/pages/LogoDesign.css";
+import "./styles/pages/PhotoFrame.css";
+import "./styles/pages/PosterDesign.css";
+import "./styles/pages/Price.css";
+import "./styles/pages/ServicePage.css";
+import "./styles/pages/SocialMediaBanner.css";
+import "./styles/pages/UIDesign.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
